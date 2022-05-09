ed9081ee4d5f6aad638e8b9c83ec65c36bb56263
SebastianZen <91784084+SebastianZen@users.noreply.github.com>
2022-05-09 18:18:30 +0200

    UserLogsController complete

--- ZenBuilds/Controllers/UserLogsController.cs
+++ ZenBuilds/Controllers/UserLogsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ZenBuilds.Authorization;
+using ZenBuilds.Models.UserLogs;
 using ZenBuilds.Services;
 
 namespace ZenBuilds.Controllers;
@@ -14,5 +15,47 @@ public class UserLogsController : ControllerBase
     public UserLogsController(IUserLogService userLogService)
     {
         _userLogService = userLogService;
+    }
+
+    [HttpPost("logAuthentication")]
+    public IActionResult LogAuthentication(LogAuthenticateRequest logAuthenticateRequest)
+    {
+        try
+        {
+            _userLogService.LogAuthentication(logAuthenticateRequest);
+            return Ok(new { message = "Authentication logged" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("getAllLogs")]
+    public IActionResult GetAllLogs()
+    {
+        var allLogs = _userLogService.GetAllLogs();
+        return Ok(allLogs);
+    }    
+
+    [HttpGet("getAllLogsByUserId")]
+    public IActionResult GetAllLogsByUserId(int userId)
+    {
+        var allLogsByUserId = _userLogService.GetAllLogsByUserId(userId);
+        return Ok(allLogsByUserId);
+    }    
+
+    [HttpGet("getAllSuccessfulAuthenticationsByUserId")]
+    public IActionResult GetAllSuccessfulAuthenticationsByUserId(int userId)
+    {
+        var allSuccessfulAuthenticationsByUserId = _userLogService.GetAllSuccessfulAuthenticationsByUserId(userId);
+        return Ok(allSuccessfulAuthenticationsByUserId);
+    }
+
+    [HttpGet("getAllFailedAuthenticationsByUserId")]
+    public IActionResult GetAllFailedAuthenticationsByUserId(int userId)
+    {
+        var allFailedAuthenticationsByUserId = _userLogService.GetAllFailedAuthenticationsByUserId(userId);
+        return Ok(allFailedAuthenticationsByUserId);
     }
 }
