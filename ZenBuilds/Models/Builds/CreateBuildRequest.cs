3949c816da59e015fbed78a35bca45741af5690c
SebastianSchvn <sebastian.schavon@hotmail.se>
2022-05-25 22:00:26 +0200

    Sqlite migrations folder for mac. Attribute changes to CreateBuild

--- ZenBuilds/Models/Builds/CreateBuildRequest.cs
+++ ZenBuilds/Models/Builds/CreateBuildRequest.cs
@@ -6,11 +6,11 @@ namespace ZenBuilds.Models.Builds;
 public class CreateBuildRequest
 {
     [Required]
-    [MaxLength(250, ErrorMessage = "Title cannot exceed more than 50 characters"), MinLength(8, ErrorMessage = "Title must be more than 4 characters")]
+    [MaxLength(150, ErrorMessage = "Title cannot exceed more than 150 characters"), MinLength(6, ErrorMessage = "Title must be more than 6 characters")]
     public string Title { get; set; }
 
     [Required]
-    [DataType(DataType.MultilineText), MaxLength(250, ErrorMessage = "Build cannot exceed more than 250 characters"), MinLength(8, ErrorMessage = "Build must be more than 8 characters")]
+    [DataType(DataType.MultilineText), MaxLength(2500, ErrorMessage = "Build cannot exceed more than 2500 characters"), MinLength(8, ErrorMessage = "Build must be more than 8 characters")]
     public string Content { get; set; }
 
     [Required]
