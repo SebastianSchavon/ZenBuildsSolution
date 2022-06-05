7fe591e9780af3612ff659c5906a7669bbc65597
SebastianSchvn <sebastian.schavon@hotmail.se>
2022-06-05 10:56:07 +0200

    UserLog DTO implemented

---
+++ ZenBuilds/Models/UserLogs/UserLogResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ZenBuilds.Models.UserLogs;
+
+public class UserLogResponse
+{
+   
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Ip { get; set; }
+    public bool AuthSuccessful { get; set; }
+
+    public string Date { get; set; }
+}
+
